a7cd87a6786a0051a2cae340933acab98d128b09
Devanshu <devanshukd007@gmail.com>
2023-06-08 18:51:32 +0530

    Print like: 1 2 3 4 5 5 4 3 2 1

--- Recursion_01_Basics/Mix_Of_Front_Back_Counting_01.java
+++ Recursion_01_Basics/Mix_Of_Front_Back_Counting_01.java
@@ -2,7 +2,7 @@ package Recursion_01_Basics;
 
 public class Mix_Of_Front_Back_Counting_01 {
 	
-	// printing : 1 2 3 4 5 5 4 3 2 1
+	// Printing : 1 2 3 4 5 5 4 3 2 1
 
 	public static void main(String[] args) {
 
