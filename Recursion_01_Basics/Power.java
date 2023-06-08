c8d0cc7c076dd66eb8c5de83603cdc6a39f58cee
Devanshu <devanshukd007@gmail.com>
2023-06-08 18:53:27 +0530

    Power Function by Using Recursion

--- Recursion_01_Basics/Power.java
+++ Recursion_01_Basics/Power.java
@@ -3,21 +3,21 @@ package Recursion_01_Basics;
 public class Power {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(pow(2, 5 ));
+
+		System.out.println(pow(2, 5)); // 2^5
 
 	}
-	
+
 	public static int pow(int num, int power) {
-		
-		// base case would be 
-		if(power == 1) {
+
+		// base case would be
+		if (power == 1) {
 			return num; // because num will be remain same
 		}
-		
-		int ans = num * pow(num, power - 1);
+
+		int ans = num * pow(num, power - 1); 
 		return ans;
-		
+
 	}
 
 }
